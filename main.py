052baf0368cf0c1660df07b2fe60c95ec9b0ab74
tarodo <rsvolozhanin@gmail.com>
2024-03-15 22:43:34 +0400

    fix: add user auth and reg

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
 
 API_URL = os.getenv("API_URL")
 
@@ -52,21 +53,20 @@ def random_email() -> str:
 
 def get_bot_token(bot_email, bot_pass):
     """Get bot token for the api"""
+    logger.debug("bot api token :: start")
     api_token = ""
     while not api_token:
         url = f"{API_URL}/login/access-token"
         data = {"username": bot_email, "password": bot_pass}
-        token = None
         try:
+            logger.debug(f"bot api token :: {url=} :: {data=}")
             res = requests.post(url, data=data)
-            token = res.json()
             res.raise_for_status()
-        except requests.exceptions.ConnectionError:
-            logger.error(f"ConnectionError in bot api token gen :: {token}")
-            sleep(2)
-
-        else:
             api_token = res.json()["access_token"]
+            logger.debug(f"bot api token :: {api_token}")
+        except requests.exceptions.ConnectionError:
+            logger.error(f"ConnectionError in bot api token gen")
+            sleep(2)
     return api_token
 
 
@@ -78,44 +78,92 @@ def keyboard_maker(buttons, number):
     return markup
 
 
-async def get_user_token(
-    user_id: int, context: ContextTypes.DEFAULT_TYPE
-) -> str | None:
-    user_token = None
-    if not context or not context.user_data:
+async def get_user_token(user_id: int, bot_token: str) -> str | None:
+    logger.debug("user api token :: start")
+    url = f"{API_URL}/login/access-token-bot"
+    headers = {"Authorization": f"Bearer {bot_token}"}
+    data = {"tg_id": user_id}
+    res_body = None
+    try:
+        logger.debug(f"user api token :: {url=} : {data=} :: {headers=}")
+        res = requests.post(url, json=data, headers=headers)
+        res.raise_for_status()
+        user_token = res.json()["access_token"]
+        logger.debug(f"user api token :: {user_token}")
+    except Exception:
+        logger.debug(f"user api token :: empty :: {res_body}")
+        return None
+    logger.debug("user api token :: finish")
+    return user_token
+
+
+async def reg_user(user_id: int, bot_token: str) -> str | None:
+    logger.debug("user reg :: start")
+    url = f"{API_URL}/users/"
+    headers = {"Authorization": f"Bearer {bot_token}"}
+    data = {
+        "email": random_email(),
+        "tg_id": user_id,
+        "password": random_lower_string(),
+    }
+    res_body = None
+    try:
+        logger.debug(f"user reg :: {url=} : {data=} :: {headers=}")
+        res = requests.post(url, json=data, headers=headers)
+        res_body = res.text
+        res.raise_for_status()
+        return await get_user_token(user_id, bot_token)
+    except Exception:
+        logger.debug(f"user reg :: empty :: {res_body}")
         return None
 
-    user_token = context.user_data.get("user_token")
-    bot_token = context.bot_data.get("bot_token")
-    if not user_token:
-        url = f"{API_URL}/login/access-token-bot"
-        headers = {"Authorization": f"Bearer {bot_token}"}
-        data = {"tg_id": user_id}
-        try:
-            res = requests.post(url, data=data, headers=headers)
-            res.raise_for_status()
-        except Exception:
-            return None
-        else:
-            user_token = res.json()["access_token"]
-            context.user_data["user_toker"] = user_token
 
-    return user_token
+async def main_menu(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Send first question for the user"""
+    if not update.message or not update.message.from_user:
+        return ConversationHandler.END
+    if not context or not context.user_data:
+        return ConversationHandler.END
+
+    text = "Hello my friend! What do you want from me?"
+    buttons = ["Words"]
+    markup = keyboard_maker(buttons, 2)
+    message_id = await update.message.reply_text(
+        text,
+        reply_markup=markup,
+    )
+    context.user_data["active_message"] = message_id
+
+    return StateEnum.CHOOSING_ACT
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
     """Start the conversation and ask user for input."""
     if not update.message or not update.message.from_user:
         return ConversationHandler.END
+    if not context:
+        return ConversationHandler.END
 
     user_id = update.message.from_user.id
     if not user_id:
         return ConversationHandler.END
-    user_token = get_user_token(user_id, context)
+
+    user_token = context.user_data.get("user_token") if context.user_data else None
+    bot_token = context.bot_data.get("bot_token") if context.bot_data else None
+    logger.debug(f"start :: {user_token=} :: {bot_token=}")
+    if not user_token and bot_token:
+        user_token = await get_user_token(user_id, bot_token)
+    if user_token:
+        if context.user_data is None:
+            return ConversationHandler.END
+        context.user_data["user_token"] = user_token
+
     if not user_token:
         await update.message.reply_text("You do not have a token")
+        return ConversationHandler.END
+
     await update.message.reply_text(f"Success!! {user_id}")
-    return StateEnum.CHOOSING_ACT
+    return await main_menu(update, context)
 
 
 async def cancel(update: Update, _: ContextTypes.DEFAULT_TYPE) -> int:
@@ -136,19 +184,16 @@ def main() -> None:
     bot_token = os.getenv("BOT_TOKEN")
     if not bot_token:
         return None
-    logger.info("bot token :: finish")
     application = Application.builder().token(bot_token).build()
 
-    logger.info("bot api token :: start")
     bot_email = os.getenv("BOT_EMAIL")
     bot_pass = os.getenv("BOT_PASS")
     api_token = get_bot_token(bot_email, bot_pass)
-    logger.info("bot api token :: finish")
     if not api_token:
         logger.info("bot api token :: empty")
         return None
 
-    application.bot_data["api_token"] = api_token
+    application.bot_data["bot_token"] = api_token
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler("start", start)],
